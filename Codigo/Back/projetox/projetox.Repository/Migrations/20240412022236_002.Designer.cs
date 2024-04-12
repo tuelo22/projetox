3cd11909d1ae83f608fa45ac43345534103705a5
Julio Francis Pereira dos Santos Mendonça <julio_fpsm@hotmail.com>
2024-04-11 23:24:59 -0300

    Incluindo inserção das naturezas juridicas.

--- Codigo/Back/projetox/projetox.Repository/Migrations/20240412022236_002.Designer.cs
+++ Codigo/Back/projetox/projetox.Repository/Migrations/20240412022236_002.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using projetox.Repository;
 
@@ -11,9 +12,11 @@ using projetox.Repository;
 namespace projetox.Repository.Migrations
 {
     [DbContext(typeof(XContext))]
-    partial class XContextModelSnapshot : ModelSnapshot
+    [Migration("20240412022236_002")]
+    partial class _002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
